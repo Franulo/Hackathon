67518e9c98b362442638f1fc4fde06585874ac40
Franulo <franciszek@balamut.eu>
2025-05-10 14:57:00 +0200

    hello hello

---
+++ frontend/lib/inputfield.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Inputfield extends StatefulWidget {
+  const Inputfield({super.key});
+
+  @override
+  State<Inputfield> createState() => _InputfieldState();
+}
+
+class _InputfieldState extends State<Inputfield> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
